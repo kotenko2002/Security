de6b284775e4f9100cae8202dddb84d5e1f8540e
Kotenko Dmytro <kotenko2002@gmail.com>
2023-09-24 16:40:28 +0300

    fix

--- lab2/basicTask.js
+++ lab2/basicTask.js
@@ -19,5 +19,33 @@ request(getTokenRequest, (error, response, body) => {
     return;
   }
 
-  console.log(JSON.parse(body));
+  const info = JSON.parse(body);
+  console.log("step #1:", info);
+
+  const createUser = {
+    method: "POST",
+    url: `https://${process.env.BASIC_DOMAIN}/api/v2/users`,
+    headers: {
+      "content-type": "x-www-form-urlencoded",
+      Authorization: `Bearer ${info.access_token}`,
+    },
+    form: {
+      email: "kotenkoD01@gmail.com",
+      given_name: "Dmytro",
+      family_name: "Kotenko",
+      name: "Dmytro Kotenko",
+      nickname: "kotenkoD01",
+      connection: "Username-Password-Authentication",
+      password: "kotenkoD01",
+    },
+  };
+
+  request(createUser, function (error, response, body) {
+    if (error) {
+      console.log("error in step #2:", error);
+      return;
+    }
+
+    console.log("step #2:", JSON.parse(body));
+  });
 });
